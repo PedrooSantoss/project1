cd0aa88354ae113cd0a64e52444b553a0f2cb07b
PedrooSantoss <pedromiguel.senai564@gmail.com>
2023-08-17 17:08:00 -0300

    feat: add display function

--- assets/javascript/script.category.js
+++ assets/javascript/script.category.js
@@ -23,21 +23,21 @@ class CategoryService {
         this.nextCategoryId = 0;
     }
     addCategory(name) {
-        const id = this.nextCategoryId ++;
+        const id = this.nextCategoryId++;
         const category = new Category(id, name);
         this.categories.push(category);
 
     }
 }
 
-class ProductService{
+class ProductService {
     constructor() {
         this.products = [];
         this.nextProductId = 0;
     }
 
     addProduct(name, price, category) {
-        const id = this.nextProductId ++;
+        const id = this.nextProductId++;
         const product = new Product(id, name, price, category);
         category.products.push(product);
         this.products.push(product);
@@ -49,27 +49,64 @@ const productList = new ProductService();
 
 function createCategory() {
     const categoryName = document.getElementById("categoryName");
-   //console.log(categoryName)
+    //console.log(categoryName)
 
     categoryList.addCategory(categoryName);
-
+    clearformFields
     console.log(categoryList.categories);
+    displayCategoriesAndProducts()
 }
 
-function createProduct(){
+function createProduct() {
     const productName = "bolo";
     const productPrice = 20;
     const productCategory = categoryList.categories[0];
 
     productList.addProduct(productName, productPrice, productCategory);
 
-   // console.log(productList.products);
+    // console.log(productList.products);
 }
 
-function clearformFields(){
+function clearformFields() {
     document.getElementById("categoryName").value = "";
-    document.getElementById("categoryName").value = "";
-    document.getElementById("categoryPrice").value = "";
-    document.getElementById("categoryCategory").value = "";
+    document.getElementById("productName").value = "";
+    document.getElementById("productPrice").value = "";
+    document.getElementById("productCategory").value = "";
 
+}
+
+function displayCategoriesAndProducts() {
+    let content = "";
+
+    categoryList.categories.forEach((category) => {
+        content += `
+        <li>
+            <div class="categoriesList">
+            <span><b>Categoria:</b>${category.name}</span>
+            <div>
+                <button class="editButton">Editar</button>
+                <button class="deleteButton">Remover</button>
+            </div>
+            </div>
+
+            <ul class="productsListByCategory">;
+            category.products.forEach((product) => {
+                content += `
+                <li>
+                    <div class="productsList">
+                        <span><b>Produto:</b> ${product.name} - <b>Preço:</b></span>`
+                        <div>
+                        <button class="editButton">Editar</button>
+                        <button class="deleteButton">Remover</button>
+                        </div>
+                    </div>
+                </li>
+            })
+        
+    });
+    content += `
+    </ul>
+    </li>
+    ;
+    document.getElementById("categoriesList").innerHTML = content;
 }
