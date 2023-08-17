e50e871e324b0f540876c51c2f6b66193206814d
PedrooSantoss <pedromiguel.senai564@gmail.com>
2023-08-17 10:18:10 -0300

    feat: add class product

--- assets/javascript/script.category.js
+++ assets/javascript/script.category.js
@@ -6,3 +6,13 @@ class Category {
         this.products = [];
     }
 }
+
+// Produtos pertence a uma categoria => 1:1
+class Category {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
