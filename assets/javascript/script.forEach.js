22936b91ed5e18fb82979f3b5996e3685268d86e
PedrooSantoss <pedromiguel.senai564@gmail.com>
2023-08-17 10:05:17 -0300

    ....

---
+++ assets/javascript/script.forEach.js
@@ -0,0 +1,28 @@
+/* 
+const companies = ["Apple", "Google", "Facebook"];
+
+companies.forEach(company => {
+    console.log(`Hey, ${company}!`);
+});
+*/
+/*
+const names = ["Whinds", "Freeway", "Teste", "Maria"];
+
+names.forEach((name) =>{
+    console.log(`Hi, ${name}!`);
+});
+*/
+/*
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers);
+
+numbers.forEach((number, index) => {
+    if (number % 2 == 0) {
+        console.log(`${number} está na posição ${index} do array`);
+    }
+})
+*/
+
+// 
+
